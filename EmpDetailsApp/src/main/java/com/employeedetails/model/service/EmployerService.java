e2a171637a58138229f03aeea69e31a22dd1cd31
pavithrachamala <56586609+pavithrachamala@users.noreply.github.com>
2020-04-19 17:27:27 +0530

    Update EmployerService.java

--- EmpDetailsApp/src/main/java/com/employeedetails/model/service/EmployerService.java
+++ EmpDetailsApp/src/main/java/com/employeedetails/model/service/EmployerService.java
@@ -1,9 +1,6 @@
 package com.employeedetails.model.service;
-
 import java.util.List;
-
 import com.employeedetails.model.entities.Employer;
-
 public interface EmployerService {
 	public List<Employer> findAll();
 	public Employer findByIdEmployer(Long id);
