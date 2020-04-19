9e95a1821adddc035f4e36b9320ed8d33ddf4e1e
pavithrachamala <56586609+pavithrachamala@users.noreply.github.com>
2020-04-19 17:29:15 +0530

    Update EmployerRestController.java

--- EmpDetailsApp/src/main/java/com/employeedetails/web/controller/EmployerRestController.java
+++ EmpDetailsApp/src/main/java/com/employeedetails/web/controller/EmployerRestController.java
@@ -1,7 +1,5 @@
 package com.employeedetails.web.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.employeedetails.model.entities.Employer;
 import com.employeedetails.model.service.EmployerService;
 @RestController
